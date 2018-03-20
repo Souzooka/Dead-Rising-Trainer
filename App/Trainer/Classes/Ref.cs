52981504d3269cbffc230bd3570a756f6513760e
Souzooka <bazookaharwell@gmail.com>
2018-03-20 04:38:11 -1000

    Add generic Ref class to represent pointers which do not require special write/read code

---
+++ App/Trainer/Classes/Ref.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Trainer.ComponentUtil;
+
+namespace Trainer.Classes
+{
+    public class Ref<T> where T : struct
+    {
+        private IntPtr ptr;
+
+        public T Deref(Process process)
+        {
+            return process.ReadValue<T>(ptr);
+        }
+
+        public bool Write(Process process, T obj)
+        {
+            return process.WriteValue<T>(ptr, obj);
+        }
+    }
+}
